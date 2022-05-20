ea19d91e17c85298bd9663b71c958b879dfcf2d6
Rüçhan Adıgüzel <jceadsa@gmail.com>
2022-05-20 23:46:07 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var channel = WebSocketChannel.connect(Uri.parse("wss://localhost:3000"));
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: channel.stream,
+        builder: (context, snapshot) {
+          return ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) => Card(
+                    child: Text(snapshot.data.toString()),
+                  ));
+        },
+      ),
+    );
+  }
+}
